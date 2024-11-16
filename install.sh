ccd74940dc415163e9bc5b2fde6e3f4f667c3394
Vinicius Mayrink <vmayrink12@gmail.com>
2024-11-15 21:02:52 -0300

    feat: updates script to be called with curl

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CREDS_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/creds.json.enc
+CONFIG_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/config.json
+
+curl -L -O $CREDS_FILE
 openssl enc -aes-256-cbc -d -in creds.json.enc -out creds.json -pbkdf2
 pacman -S archinstall
-archinstall --config config.json --creds creds.json
+archinstall --config $CONFIG_FILE --creds creds.json
