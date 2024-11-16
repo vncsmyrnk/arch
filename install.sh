1a179ecea269495cf78f9d6178895779ea93cad9
Vinicius Mayrink <vmayrink12@gmail.com>
2024-11-15 21:15:08 -0300

    fix: updates files URLs

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CREDS_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/creds.json.enc
-CONFIG_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/config.json
+CREDS_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/refs/heads/main/creds.json.enc
+CONFIG_FILE=https://raw.githubusercontent.com/vncsmyrnk/arch/refs/heads/main/config.json
 
 curl -L -O $CREDS_FILE
 openssl enc -aes-256-cbc -d -in creds.json.enc -out creds.json -pbkdf2
